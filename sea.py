78d061f3384f448b057a011e6f2f630557d05c4d
data-man00 <108255830+data-man00@users.noreply.github.com>
2022-07-11 23:58:07 +0530

    Delete sea.py

--- sea.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
-
-df = pd.read_csv('epa-sea-level.csv',header=0)
-plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
-plt.show()
